fabf6b56c5d09f97d38c56c4e06c42b167658ebb
Asmaa <asmaakhdr24@gmail.com>
2022-12-30 18:31:29 +0200

    add movie cmp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -12,7 +13,10 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.py4e.com/api/films');
+      const response = await fetch(
+        'https://react-http-ffae2-default-rtdb.firebaseio.com//movies.json'
+      );
+      console.log(response);
 
       if (!response.ok) {
         throw new Error('Something Went Wrong!');
@@ -40,8 +44,15 @@ function App() {
     fetchMoviesHandler();
   }, []);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
